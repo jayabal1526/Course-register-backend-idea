9b1210969137819a4d86841b03839ccf9d7b592c
jayabal1526 <132429994+jayabal1526@users.noreply.github.com>
2025-04-27 21:07:49 +0530

    init

---
+++ src/main/java/com/example/CourseRegister/CourseRegisterApplication.java
@@ -0,0 +1,12 @@
+package com.example.CourseRegister;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+
+public class CourseRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseRegisterApplication.class, args);
+	}
+
+}
